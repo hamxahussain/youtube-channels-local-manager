4092f92137dcab44a0b4254841b87e9669e272d2
Hamza Hussain <hamza.xeros@gmail.com>
2021-10-11 12:13:39 +0500

    📦 Package 1.0.0 is deployed to NPM successfully

--- src/global.js
+++ src/global.js
@@ -1,22 +1,26 @@
-const path = require("path")
+const path = require("path");
 
-module.exports.YOUTUBE_DIR_NAME = "Youtubehannels"
 module.exports.GIT_YOUTUBE_DIR_NAME = ".git_youtube_channels_resources";
-module.exports.YOUTUBE_DIR_FULL = path.join(process.env.HOME, this.YOUTUBE_DIR_NAME)
-module.exports.GIT_YOUTUBE_DIR_FULL = path.join(process.env.HOME, this.GIT_YOUTUBE_DIR_NAME)
+module.exports.YOUTUBE_DIR_FULL = path.join(
+	process.env.HOME,
+	this.YOUTUBE_DIR_NAME
+);
+module.exports.GIT_YOUTUBE_DIR_FULL = path.join(
+	process.env.HOME,
+	this.GIT_YOUTUBE_DIR_NAME
+);
 module.exports.MONTHS = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
 ];
 module.exports.nameRegex = /[^ ][a-zA-Z0-9-_]/g;
-
